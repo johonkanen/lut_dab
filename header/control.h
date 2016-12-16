739ab2286a8c78084d71b22f014373bdf3082d33
Jari Honkanen <jari.honkanen@lutfi>
2016-12-16 15:33:36 +0200

    make pi init more readable

--- header/control.h
+++ header/control.h
@@ -11,7 +11,15 @@
 #include "ctrl_macros.h"
 
 //pid_controller is the object for the pid controller for a specific control task
-void init_f_pid_control(struct f_pid_ctrl* pid_controller, float kp, float ki, float kd, float kf, float ref,Uint16* ctrl_measurement,float(*control_function)(struct f_pid_ctrl*));
+void init_f_pid_control(struct f_pid_ctrl* pid_controller,
+						float kp,//p gain
+						float ki,//I gain
+						float kd,//D gain
+						float kf,//D filter gain
+						float ref,// reference
+						Uint16* ctrl_measurement,//Uint16* pointer to measurement
+						float(*control_function)(struct f_pid_ctrl*)//control function pointer
+						);
 
 float fpid_control(struct f_pid_ctrl*);
 
