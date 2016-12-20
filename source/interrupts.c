5cf2c4b04a1a23aba82d2407dda67ced90274b15
Jari Honkanen <jari.honkanen@lutfi>
2016-12-20 14:17:53 +0200

    add offset for pri and sec offset

--- source/interrupts.c
+++ source/interrupts.c
@@ -13,8 +13,11 @@
 #pragma CODE_SECTION(PWM1_int, "ramfuncs");
 
 float ctrl_scaled;
-float duty1 = .1;
-float duty2 = .2;
+float duty1 = .5;
+float duty2 = 1;
+float ctrl;
+float p_offset;
+float s_offset;
 
 __interrupt void PWM1_int(void)
 {
@@ -26,15 +29,30 @@ __interrupt void PWM1_int(void)
 
 	// macro for calling a function through a pointer
 
-	ctrl_scaled= m_execute_fpid_ctrl(voltage_ctrl);
+	ctrl= m_execute_fpid_ctrl(voltage_ctrl);
+
+	if (ctrl>0)
+	{
+		p_offset = 0;
+		s_offset = 1;
+	}
+	else if(ctrl<0)
+	{
+		p_offset = 1;
+		s_offset = 0;
+	}
+	else if(ctrl == 0)
+	{
+		p_offset = 0;
+		s_offset = 0;
+	}
 
 	if(duty1>duty2)
-		ctrl_scaled = (ctrl_scaled*223)*duty1+226*(1-duty1*duty2);
+		ctrl_scaled = (ctrl*223)*duty1+226*(duty2);
 	else
-		ctrl_scaled = (ctrl_scaled*223)*duty2+226*(1-duty1*duty2);
+		ctrl_scaled = (ctrl*223)*duty2+226*(duty1);
 
 	// the 223 and 226 are used to scale the +/-1 output of PI control to phase register values
-	//ctrl_scaled = (ctrl_scaled*223)*duty1+226*(1-duty1*duty2);
 
 	ph_shift_1 = -225+(225+225*(1-duty1));
 	ph_shift_2 =  225-(225-225*(1-duty1));
@@ -46,11 +64,11 @@ __interrupt void PWM1_int(void)
 	ph_shift_pri_sec_2 =  225-(ctrl_scaled);
 
 
-	*phase_reg.p1_phase = 225+ph_shift_1+ph_shift_pri_sec_1;
-	*phase_reg.p2_phase = 225-ph_shift_2+ph_shift_pri_sec_1;
+	*phase_reg.p1_phase = p_offset*225+ph_shift_1+ph_shift_pri_sec_1;
+	*phase_reg.p2_phase = p_offset*225-ph_shift_2+ph_shift_pri_sec_1;
 
-	*phase_reg.s1_phase = 225+ph_shift_3+ph_shift_pri_sec_2;
-	*phase_reg.s2_phase = 225-ph_shift_4+ph_shift_pri_sec_2;
+	*phase_reg.s1_phase = s_offset*225+ph_shift_3+ph_shift_pri_sec_2;
+	*phase_reg.s2_phase = s_offset*225-ph_shift_4+ph_shift_pri_sec_2;
 	//GpioDataRegs.GPACLEAR.bit.GPIO17 = 1;
 	if (SciaRegs.SCIFFTX.bit.TXFFST == 0)
 	    {
