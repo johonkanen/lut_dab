51ce7dc6ba32557c7575490d68843f589c8b605b
Jari Honkanen <jari.honkanen@lutfi>
2016-12-27 15:23:38 +0200

    fix bug with wrong register update when phase <-1

--- source/interrupts.c
+++ source/interrupts.c
@@ -26,7 +26,7 @@ __interrupt void PWM1_int(void)
 
 	phase = m_execute_fpid_ctrl(voltage_ctrl);
 	duty1 = .2;
-	duty2 = .1;
+	duty2 = .3;
 
 	if (phase >=0)
 	{
@@ -55,13 +55,13 @@ __interrupt void PWM1_int(void)
 			*phase_reg.p1_phase = 449*duty1;
 			*phase_reg.p2_phase = 0;
 
-			*phase_reg.s1_phase = 449*duty2+449*duty2*phase;
-			*phase_reg.s2_phase = 0+449*duty2*phase;
+			*phase_reg.s1_phase = 449*duty2+449*duty1*phase;
+			*phase_reg.s2_phase = 0+449*duty1*phase;
 		}
 		else
 		{
-			*phase_reg.p1_phase = 449*duty1+ 449*duty1*phase;
-			*phase_reg.p2_phase = 0+ 449*duty1*phase;
+			*phase_reg.p1_phase = 449*duty1+ 449*duty2*phase;
+			*phase_reg.p2_phase = 0+ 449*duty2*phase;
 
 			*phase_reg.s1_phase = 449*duty2;
 			*phase_reg.s2_phase = 0;
