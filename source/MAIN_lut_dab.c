41cf8358607166847ca5899ef6efc8eb0f2fa447
Jari Honkanen <jari.honkanen@lutfi>
2017-02-07 11:30:05 +0200

    remove unnecessary empty lines

--- source/MAIN_lut_dab.c
+++ source/MAIN_lut_dab.c
@@ -96,10 +96,6 @@ main(void)
    current_samples.a12 = (Uint16*)&(AdcResult.ADCRESULT14);
    current_samples.a13 = (Uint16*)&(AdcResult.ADCRESULT15);
 
-
-
-
-
    meas.pri_current_lp = (Uint16*)&current_filter_2_output;
 
    mailbox = (Uint16*)&meas.pri_current_lp;
